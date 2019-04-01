cdb923a971b5c5823a49396b8143b7640b2f148c
yudeguang <84558040@qq.com>
2019-04-01 18:45:24 +0800

    add getClosestVins

--- createvin.go
+++ createvin.go
@@ -2,43 +2,116 @@ package myvin
 
 import (
 	"fmt"
+	"github.com/yudeguang/stringsx"
+	"sort"
+	"strconv"
 	"strings"
 )
 
-//根据VIN8及年份代码等生成VIN码
-func CreateVinFromYearCode(vin8, yearCode, factoryCode string, vinLast6 int) (vin string, err error) {
-	for _, vin9 := range strings.Split(Vin9strs, ",") {
-		vin := vin8 + vin9 + yearCode + factoryCode + GetSerialNumber6(vinLast6)
-		if IsVinLegal(vin) {
-			return vin, nil
-		}
+//根据样本VIN码生成与之最为接近的N个VIN码
+func getClosestVinsFrom(vin string, num int) []string {
+	vins := make([]string, 0, num)
+	if len(vin) != 17 {
+		return vins
 	}
-	return "", fmt.Errorf("has illegal character in vin")
+	vin_8 := vin[0:8]
+	year_code := GetYearCodeFromVin(vin)
+	factory_code := GetFactoryCodeFromVin(vin)
+	vin_last_6 := stringsx.Right(vin, 6)
+
+	x, cur_serial_number, cur_serial_number_begin, cur_serial_number_end, err := split_vin_last_6(vin_last_6)
+
+	if err != nil {
+		return vins
+	}
+	closestSerialNumbers := getClosestSerialNumbersFrom(cur_serial_number, cur_serial_number_begin, cur_serial_number_end, num)
+	//log.Println(closestSerialNumbers)
+	//判断最后6位是否是数字
+	for _, cur_serial_number := range closestSerialNumbers {
+		vin_last_6 := get_vin_last_6_from(x, cur_serial_number)
+		vin = getOneVinFrom(vin_8, year_code, factory_code, vin_last_6)
+		vins = append(vins, vin)
+	}
+	return vins
 }
 
-//根据VIN8及年份等生成VIN码
-func CreateVinFromYear(vin8, year, factoryCode string, vinLast6 int) (vin string, err error) {
-	for _, vin9 := range strings.Split(Vin9strs, ",") {
-		vin := vin8 + vin9 + GetYearCodeFromYear(year) + factoryCode + GetSerialNumber6(vinLast6)
-		if IsVinLegal(vin) {
-			return vin, nil
+//获得最为接近的相关序列号
+func getClosestSerialNumbersFrom(cur_serial_number, cur_serial_number_begin, cur_serial_number_end, num int) []int {
+	result := make([]int, 0, num)
+	m, n := cur_serial_number, cur_serial_number
+	for {
+		n = n - 1
+		if n >= cur_serial_number_begin {
+			result = append(result, n)
+		}
+		if len(result) == num {
+			sort.Ints(result)
+			return result
+		}
+		m = m + 1
+		//算完之后，没有达到数量，也返回
+		if m > cur_serial_number_end {
+			sort.Ints(result)
+			return result
+		}
+		result = append(result, m)
+		//达到相应的数量，则返回
+		if len(result) == num {
+			sort.Ints(result)
+			return result
 		}
 	}
-	return "", fmt.Errorf("has illegal character in vin")
 }
 
-//根据区间生成多个VIN
-func CreateVins(vin8, yearCode, factoryCode string, vinLast6Begin, vinLast6End int) (ret []string) {
-	if vinLast6Begin < 1 {
-		vinLast6Begin = 1
-	}
-	if vinLast6End > 999999 {
-		vinLast6End = 999999
-	}
-	for vinLast6 := vinLast6Begin; vinLast6 <= vinLast6End; vinLast6++ {
-		if vin, err := CreateVinFromYearCode(vin8, yearCode, factoryCode, vinLast6); err == nil {
-			ret = append(ret, vin)
+func getOneVinFrom(vin8, year_code, factory_code, vin_last_6 string) string {
+	for _, vin9 := range strings.Split(Vin9strs, ",") {
+		vin := vin8 + vin9 + year_code + factory_code + vin_last_6
+		if IsVinLegal(vin) {
+			return vin
 		}
 	}
-	return ret
+	return ""
+}
+
+//生成最后6位
+func get_vin_last_6_from(x string, cur_serial_number int) string {
+	//log.Println(x, cur_serial_number)
+	s := strconv.Itoa(cur_serial_number)
+	return x + strings.Repeat("0", 6-len(s)-len(x)) + s
+}
+
+//把最后6位拆分成两个字段
+func split_vin_last_6(vin_last_6 string) (x string, cur_serial_number, serial_number_begin, serial_number_end int, err error) {
+	//前6位
+	n, err := strconv.Atoi(vin_last_6)
+	if err == nil {
+
+		return "", n, 0, 999999, nil
+	}
+	//前5位
+	n, err = strconv.Atoi(vin_last_6[1:])
+	if err == nil {
+		return vin_last_6[0:1], n, 0, 99999, nil
+	}
+	//前4位
+	n, err = strconv.Atoi(vin_last_6[2:])
+	if err == nil {
+		return vin_last_6[0:2], n, 0, 9999, nil
+	}
+	//前3位
+	n, err = strconv.Atoi(vin_last_6[3:])
+	if err == nil {
+		return vin_last_6[0:3], n, 0, 999, nil
+	}
+	//前2位
+	n, err = strconv.Atoi(vin_last_6[4:])
+	if err == nil {
+		return vin_last_6[0:4], n, 0, 99, nil
+	}
+	//前1位
+	n, err = strconv.Atoi(vin_last_6[5:])
+	if err == nil {
+		return vin_last_6[0:5], n, 0, 9, nil
+	}
+	return x, cur_serial_number, 0, 0, fmt.Errorf("此为非法VIN码")
 }
