8d83d81aa19b5571aa0e98377169d5d52efa0ed7
yudeguang <84558040@qq.com>
2019-04-03 13:34:21 +0800

    add GetVinsByRandFrom

--- createvin.go
+++ createvin.go
@@ -8,6 +8,30 @@ import (
 	"strings"
 )
 
+//根据VIN样本伪随机生成VIN码，规则为从尾数0开始每隔N个数字产生一个VIN码,every必须大于等于1小于999999
+func GetVinsByRandFrom(vin string, every int) []string {
+	vins := make([]string, 0, 999999/every)
+	if every < 1 || every > 999999 {
+		return vins
+	}
+	vin_8 := vin[0:8]
+	year_code := GetYearCodeFromVin(vin)
+	factory_code := GetFactoryCodeFromVin(vin)
+	vin_last_6 := stringsx.Right(vin, 6)
+
+	x, _, cur_serial_number_begin, cur_serial_number_end, err := split_vin_last_6(vin_last_6)
+
+	if err != nil {
+		return vins
+	}
+	for i := cur_serial_number_begin; i <= cur_serial_number_end; i = i + every {
+		vin_last_6 := get_vin_last_6_from(x, i)
+		vin = getOneVinFrom(vin_8, year_code, factory_code, vin_last_6)
+		vins = append(vins, strings.ToUpper(vin))
+	}
+	return vins
+}
+
 //根据样本VIN码生成与之最为接近的N个VIN码
 func GetClosestVinsFrom(vin string, num int) []string {
 	vins := make([]string, 0, num)
@@ -80,7 +104,8 @@ func get_vin_last_6_from(x string, cur_serial_number int) string {
 	return x + strings.Repeat("0", 6-len(s)-len(x)) + s
 }
 
-//把最后6位拆分成两个字段
+//把最后6位拆分成两个部分，0x8765 ==> "0x" 8765 0 9999
+//9876544 ==> "" 9876544 0 999999
 func split_vin_last_6(vin_last_6 string) (x string, cur_serial_number, serial_number_begin, serial_number_end int, err error) {
 	//前6位
 	n, err := strconv.Atoi(vin_last_6)
