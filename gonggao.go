391b03df0c84ef1956a6c5ab7b78ff04344da633
unknown <84558040@qq.com>
2021-07-24 14:58:12 +0800

    add FmtGonggaoNoPlus

--- gonggao.go
+++ gonggao.go
@@ -1,6 +1,8 @@
 package myvin
 
 import (
+	"strings"
+
 	"github.com/yudeguang/stringsx"
 )
 
@@ -17,4 +19,57 @@ func FmtGonggaoNo(gonggaoNo string) string {
 		lastHAN++
 	}
 	return string(r[lastHAN:])
+}
+
+//获取公告型号,去除汉字等字符
+func FmtGonggaoNoPlus(gonggaoNo string) string {
+	a := strings.ToUpper(gonggaoNo)
+	a = strings.Replace(a, ` `, ``, -1)
+	a = strings.Replace(a, `~`, ``, -1)
+	a = strings.Replace(a, `!`, ``, -1)
+	a = strings.Replace(a, `@`, ``, -1)
+	a = strings.Replace(a, `#`, ``, -1)
+	a = strings.Replace(a, `$`, ``, -1)
+	a = strings.Replace(a, `%`, ``, -1)
+	a = strings.Replace(a, `^`, ``, -1)
+	a = strings.Replace(a, `&`, ``, -1)
+	a = strings.Replace(a, `*`, ``, -1)
+	a = strings.Replace(a, `(`, ``, -1)
+	a = strings.Replace(a, `)`, ``, -1)
+	a = strings.Replace(a, `（`, ``, -1)
+	a = strings.Replace(a, `）`, ``, -1)
+	a = strings.Replace(a, `-`, ``, -1)
+	a = strings.Replace(a, `_`, ``, -1)
+	a = strings.Replace(a, `=`, ``, -1)
+	a = strings.Replace(a, `+`, ``, -1)
+	a = strings.Replace(a, `{`, ``, -1)
+	a = strings.Replace(a, `}`, ``, -1)
+	a = strings.Replace(a, `[`, ``, -1)
+	a = strings.Replace(a, `]`, ``, -1)
+	a = strings.Replace(a, `\`, ``, -1)
+	a = strings.Replace(a, `|`, ``, -1)
+	a = strings.Replace(a, `/`, ``, -1)
+	a = strings.Replace(a, `;`, ``, -1)
+	a = strings.Replace(a, `:`, ``, -1)
+	a = strings.Replace(a, `"`, ``, -1)
+	a = strings.Replace(a, `“`, ``, -1)
+	a = strings.Replace(a, `?`, ``, -1)
+	a = strings.Replace(a, `<`, ``, -1)
+	a = strings.Replace(a, `>`, ``, -1)
+	// 对于罗马字母，还需要小心替换一下,很容易用户端口输入不一致，统一替换做到一致
+	a = strings.Replace(a, `Ⅰ`, `I`, -1)
+	a = strings.Replace(a, `Ⅱ`, `II`, -1)
+	a = strings.Replace(a, `Ⅲ`, `III`, -1)
+	a = strings.Replace(a, `Ⅳ`, `IV`, -1)
+	a = strings.Replace(a, `Ⅴ`, `V`, -1)
+	a = strings.Replace(a, `Ⅵ`, `VI`, -1)
+	a = strings.Replace(a, `Ⅶ`, `VII`, -1)
+	a = strings.Replace(a, `Ⅷ`, `VIII`, -1)
+	a = strings.Replace(a, `Ⅸ`, `IX`, -1)
+	//逗号不能替，因为多个公告号是用逗号分隔的
+	//a = strings.Replace(a, `,`, ``, -1)
+	a = strings.Replace(a, `.`, ``, -1)
+	a = strings.Replace(a, `。`, ``, -1)
+	// a = strings.Replace(a, `，`, ``, -1)
+	return a
 }
